d5937f534119a9d6335d53ff367cc010ac29d6a1
Teodor <teodor1497@gmail.com>
2022-06-16 22:51:21 +0300

    add product routes

--- routes/product.js
+++ routes/product.js
@@ -57,7 +57,7 @@ router.put('/:id', verifyTokenAndAdmin, async (req, res) => {
 });
 
 //DELETE
-router.put('/:id', verifyTokenAndAdmin, async (req, res) => {
+router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
     try {
         await Product.findByIdAndDelete(req.params.id);
         res.status(200).json('Product has been deleted!');

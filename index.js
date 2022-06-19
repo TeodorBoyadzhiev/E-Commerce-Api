f859c272a7640fc4c91834a0abdb4fd848177287
Teodor <teodor1497@gmail.com>
2022-06-19 16:20:02 +0300

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const db = require("./database");
+const cors = require("cors");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 
 
@@ -15,11 +17,13 @@ async function start() {
     const app = express();
 
     await db(app);
+    app.use(cors());
     app.use('/api/auth', authRoute);
     app.use('/api/users', userRoute);
     app.use('/api/products', productRoute);
     app.use('/api/carts', cartRoute);
-    app.use('/api/orders', orderRoute)
+    app.use('/api/orders', orderRoute);
+    app.use('/api/checkout', stripeRoute)
 
     app.listen(port, () => {
         console.log('Server is running on port ' + port);

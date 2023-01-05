4fc0d905b2f1f5e5e6c009f5257c9cf63d2ce328
Teodor <teodor1497@gmail.com>
2023-01-05 22:59:01 +0200

    add recaptcha

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,19 @@ const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const { default: axios } = require("axios");
 
 //REGISTER
 router.post('/register', async (req, res) => {
+    const result = await reCaptchaValidation(req.body.token);
+    if (!result) {
+        res.status(400).json('Recaptcha error');
+        return;
+    }
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+        password: 'dasa', // HAVE TO FIX ENCRYPTING ISSUE
     });
 
     try {
@@ -29,27 +35,40 @@ router.post('/login', async (req, res) => {
         const user = await User.findOne({ username: req.body.username });
         !user && res.status(401).json('Wrong credentials!');
 
-        const hashPassword = CryptoJS.AES.decrypt(
-            user.password, process.env.PASS_SEC
-        );
-        const originalPassword = hashPassword.toString(CryptoJS.enc.Utf8);
+        // const hashPassword = CryptoJS.AES.decrypt(
+        //     user.password, process.env.PASS_SEC
+        // );
+        // const originalPassword = hashPassword.toString(CryptoJS.enc.Utf8);
 
-        originalPassword !== req.body.password && res.status(401).json('Wrong credentials!');
+        // originalPassword !== req.body.password && res.status(401).json('Wrong credentials!');
 
-        const accessToken = jwt.sign({
-            id: user._id,
-            isAdmin: user.isAdmin
-        }, process.env.JWT_SEC,
-            { expiresIn: '3d' }
-        );
+        // const accessToken = jwt.sign({
+        //     id: user._id,
+        //     isAdmin: user.isAdmin
+        // }, process.env.JWT_SEC,
+        //     { expiresIn: '3d' }
+        // );
 
         const { password, ...others } = user._doc;
-        res.status(200).json(...others, accessToken);
+        res.status(200).json(others);
     } catch (err) {
         res.status(500).json(err);
+        console.log(err)
     }
 
 });
+
+
+async function reCaptchaValidation(token) {
+    try {
+        const secret = process.env.RECAPTCHA_SECRET_KEY;
+        const response = await axios(`https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`, { method: "POST" });
+        return response.data.success;
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json(err);
+    }
+}
 
 
 module.exports = router

ba55242781c1a552d13b58f18df25ba49c7819a2
Teodor <teodor1497@gmail.com>
2023-01-18 22:12:03 +0200

    encrypt password and generate access token

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,25 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 const { default: axios } = require("axios");
 
 //REGISTER
 router.post('/register', async (req, res) => {
-    const result = await reCaptchaValidation(req.body.token);
-    if (!result) {
-        res.status(400).json('Recaptcha error');
-        return;
+    if (req.body.showRecaptcha) {
+        const result = await reCaptchaValidation(req.body.token);
+        if (!result) {
+            res.status(400).json('Recaptcha error');
+            return;
+        }
     }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: 'dasa', // HAVE TO FIX ENCRYPTING ISSUE
+        password: hashedPassword
     });
 
     try {
@@ -28,34 +33,39 @@ router.post('/register', async (req, res) => {
 
 
 //LOGIN
-
-
 router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ username: req.body.username });
         !user && res.status(401).json('Wrong credentials!');
 
-        // const hashPassword = CryptoJS.AES.decrypt(
-        //     user.password, process.env.PASS_SEC
-        // );
-        // const originalPassword = hashPassword.toString(CryptoJS.enc.Utf8);
+        const hasMatch = await bcrypt.compare(req.body.password, user._doc.password);
 
-        // originalPassword !== req.body.password && res.status(401).json('Wrong credentials!');
+        if (!hasMatch) {
+            throw new Error('Incorect password');
+        }
 
-        // const accessToken = jwt.sign({
-        //     id: user._id,
-        //     isAdmin: user.isAdmin
-        // }, process.env.JWT_SEC,
-        //     { expiresIn: '3d' }
-        // );
+        const accessToken = await generateToken(user._doc);
 
         const { password, ...others } = user._doc;
-        res.status(200).json(others);
+        
+        res.status(200).json(others, accessToken);
     } catch (err) {
         res.status(500).json(err);
         console.log(err)
     }
 
+});
+
+//LOGOUT
+router.get('/logout', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+
+        res.status(200);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 
@@ -65,9 +75,16 @@ async function reCaptchaValidation(token) {
         const response = await axios(`https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`, { method: "POST" });
         return response.data.success;
     } catch (err) {
-        console.log(err)
-        return res.status(500).json(err);
+        return err;
     }
+}
+
+async function generateToken(userData) {
+    return jwt.sign({
+        _id: userData._id,
+        username: userData.username,
+        email: userData.email
+    }, process.env.TOKEN_SECRET);
 }
 
 
